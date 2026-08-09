1953e38eca02a7419ae00bab204fc8b1ca11b245
Zoltán Palotai <zoltan.palotai@gamesforbusiness.com>
2026-08-09 20:29:58 +0200

    release: prepare v0.15.1

--- Sources/astrotool/main.swift
+++ Sources/astrotool/main.swift
@@ -9,7 +9,7 @@ guard let subcommand = arguments.first else {
 }
 
 if subcommand == "--version" {
-    print("astrotool 0.15.0")
+    print("astrotool 0.15.1")
     exit(0)
 }
 if subcommand == "--help" || subcommand == "help" {
