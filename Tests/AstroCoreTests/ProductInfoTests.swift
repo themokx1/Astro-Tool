fe1c56270ffe266bcd4ef02b3b0e0627152c5856
Zoltán Palotai <zoltan.palotai@gamesforbusiness.com>
2026-08-13 21:12:32 +0200

    build: prepare AstroTool 2.0 RC2

--- Tests/AstroCoreTests/ProductInfoTests.swift
+++ Tests/AstroCoreTests/ProductInfoTests.swift
@@ -14,11 +14,11 @@ import Testing
     @Test func publicIdentityIsStableAndReleaseReady() {
         #expect(ProductInfo.name == "AstroTool")
         #expect(ProductInfo.version == "2.0.0")
-        #expect(ProductInfo.releaseChannel == "Release Candidate 1")
+        #expect(ProductInfo.releaseChannel == "Release Candidate 2")
         #expect(ProductInfo.bundleIdentifier == "io.github.themokx1.AstroTool")
         #expect(ProductInfo.legacyBundleIdentifier == "com.zoltanpalotai.astrotool")
         #expect(Int(ProductInfo.build) != nil)
-        #expect(ProductInfo.displayVersion == "2.0.0 Release Candidate 1 (20009)")
+        #expect(ProductInfo.displayVersion == "2.0.0 Release Candidate 2 (20010)")
     }
 
     @Test func releaseConsumersUseTheSharedProductInfo() throws {
